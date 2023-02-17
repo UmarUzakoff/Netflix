d33a0b104d5f257b63e74ce17c509553066d7230
UzakoffUmar <uzakovumar338@gmail.com>
2023-02-17 18:29:06 +0500

    netflix

---
+++ src/Footer.js
@@ -0,0 +1,19 @@
+import React from "react";
+import "./Footer.css";
+
+function Footer() {
+  return (
+    <div className="footer">
+      <a
+        style={{ color: "white", textDecoration: "none" }}
+        href="https://www.instagram.com/uzakoff_u/"
+        target="_blank"
+        without rel="noreferrer"
+      >
+        <h1>Uzakoff</h1>
+      </a>
+    </div>
+  );
+}
+
+export default Footer;
